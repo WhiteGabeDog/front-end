778cfb58431eff3d8294afcc446aa137985a98a2
Nontanun Ausungnoen <nunx111@gmail.com>
2023-10-21 21:31:37 +0700

    Add navBar to MyGamePage

--- src/pages/MyGame.js
+++ src/pages/MyGame.js
@@ -1,9 +1,11 @@
 import { header, data } from '../mockData';
 import ScrollH from '../components/scrollH';
+import Navbar from '../components/Navbar';
 
 const MyGame = () => {
   return (
     <>
+      <Navbar/>
       <ScrollH data={data}/>
     </>
   );
