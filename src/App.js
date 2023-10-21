a7052c5a03ed1ecca07bf0d435c1c10b4b1761d3
NasmeenI <nasmeen2503@gmail.com>
2023-10-21 21:17:00 +0700

    my game page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React  from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
+import MyGame from './pages/MyGame';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/' element={<MyGame />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/MyGame.js
@@ -0,0 +1,12 @@
+import { header, data } from '../mockData';
+import ScrollH from '../components/scrollH';
+
+const MyGame = () => {
+  return (
+    <>
+      <ScrollH data={data}/>
+    </>
+  );
+}
+  
+export default MyGame;
